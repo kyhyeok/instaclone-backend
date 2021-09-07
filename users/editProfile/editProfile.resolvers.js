03ec37b751c16a4a67fe41d22ff114cc25c49f14
Kim Yoon Hyeok <kyhyeok@gmail.com>
2021-09-07 21:57:27 +0900

    #4.7 updateProfile

--- users/editProfile/editProfile.resolvers.js
+++ users/editProfile/editProfile.resolvers.js
@@ -4,6 +4,36 @@ import client from "../../client";
 
 export default {
   Mutation: {
-    editProfile: () => console.log("hi"),
+    editProfile: async (
+      _,
+      { firstName, lastName, username, email, password: newPassword }
+    ) => {
+      let uglyPassword = null;
+      if (newPassword) {
+        uglyPassword = await bcrypt.hash(newPassword, 10);
+      }
+      const updateUser = await client.user.update({
+        where: {
+          id: 1,
+        },
+        data: {
+          firstName,
+          lastName,
+          username,
+          email,
+          ...(uglyPassword && { password: uglyPassword }),
+        },
+      });
+      if (updateUser.id) {
+        return {
+          ok: true,
+        };
+      } else {
+        return {
+          ok: false,
+          error: "Could not update profile",
+        };
+      }
+    },
   },
 };
