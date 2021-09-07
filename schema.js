c2479f8a2ae5eaaf84722444eb0a8e2ca977320a
Kim Yoon Hyeok <kyhyeok@gmail.com>
2021-09-07 21:35:29 +0900

    #4.6 Divide and Conquer

--- schema.js
+++ schema.js
@@ -3,9 +3,7 @@ const { loadFilesSync } = require("@graphql-tools/load-files");
 const { mergeTypeDefs, mergeResolvers } = require("@graphql-tools/merge");
 
 const loadedTypes = loadFilesSync(`${__dirname}/**/*.typeDefs.js`);
-const loadedResolvers = loadFilesSync(
-  `${__dirname}/**/*.{queries,mutations}.js`
-);
+const loadedResolvers = loadFilesSync(`${__dirname}/**/*.resolvers.js`);
 
 const typeDefs = mergeTypeDefs(loadedTypes);
 const resolvers = mergeResolvers(loadedResolvers);
---
+++ users/editProfile/editProfile.resolvers.js
@@ -0,0 +1,9 @@
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import client from "../../client";
+
+export default {
+  Mutation: {
+    editProfile: () => console.log("hi"),
+  },
+};
