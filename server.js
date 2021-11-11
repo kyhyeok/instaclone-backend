fb9d6dc18151d594e9a0965f796cacd591ab38e7
김윤혁 <kyhyeok@gmail.com>
2021-11-11 14:09:18 +0900

    #6.12 seeFeed

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
-require('dotenv').config()
-import express from 'express'
-import logger from 'morgan'
-import { ApolloServer } from 'apollo-server-express'
-import { ApolloServerPluginLandingPageGraphQLPlayground } from 'apollo-server-core'
+require('dotenv').config();
+import express from 'express';
+import logger from 'morgan';
+import { ApolloServer } from 'apollo-server-express';
+import { ApolloServerPluginLandingPageGraphQLPlayground } from 'apollo-server-core';
 const {
     graphqlUploadExpress, // A Koa implementation is also exported.
-} = require('graphql-upload')
-import schema, { typeDefs, resolvers } from './schema'
-import { getUser } from './users/users.utils'
+} = require('graphql-upload');
+import schema, { typeDefs, resolvers } from './schema';
+import { getUser } from './users/users.utils';
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT;
 
 const startServer = async () => {
     const apolloServer = new ApolloServer({
@@ -18,27 +18,27 @@ const startServer = async () => {
         context: async ({ req }) => {
             return {
                 loggedInUser: await getUser(req.headers.token),
-            }
+            };
         },
         plugins: [ApolloServerPluginLandingPageGraphQLPlayground()],
-    })
+    });
 
-    await apolloServer.start()
+    await apolloServer.start();
 
-    const app = express()
+    const app = express();
     // app.use(logger("tiny"))
 
-    app.use(graphqlUploadExpress())
+    app.use(graphqlUploadExpress());
 
-    apolloServer.applyMiddleware({ app })
+    apolloServer.applyMiddleware({ app });
 
-    app.use('/static', express.static('uploads'))
+    app.use('/static', express.static('uploads'));
 
-    await new Promise((r) => app.listen({ port: PORT }, r))
+    await new Promise((r) => app.listen({ port: PORT }, r));
 
     console.log(
         `🚀 apolloServer ready at http://localhost:${PORT}${apolloServer.graphqlPath}`
-    )
-}
+    );
+};
 
-startServer()
+startServer();
