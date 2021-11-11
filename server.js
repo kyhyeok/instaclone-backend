6056d1ffcedefd3c9c8d858dfaa11472c5546220
김윤혁 <kyhyeok@gmail.com>
2021-11-11 10:57:42 +0900

    #6.11 seeLikes

--- server.js
+++ server.js
@@ -1,45 +1,44 @@
-require("dotenv").config();
-import express from "express";
-import logger from "morgan";
-import { ApolloServer } from "apollo-server-express";
-import { ApolloServerPluginLandingPageGraphQLPlayground } from "apollo-server-core";
+require('dotenv').config()
+import express from 'express'
+import logger from 'morgan'
+import { ApolloServer } from 'apollo-server-express'
+import { ApolloServerPluginLandingPageGraphQLPlayground } from 'apollo-server-core'
 const {
-  graphqlUploadExpress, // A Koa implementation is also exported.
-} = require("graphql-upload");
-import schema, { typeDefs, resolvers } from "./schema";
-import { getUser } from "./users/users.utils";
+    graphqlUploadExpress, // A Koa implementation is also exported.
+} = require('graphql-upload')
+import schema, { typeDefs, resolvers } from './schema'
+import { getUser } from './users/users.utils'
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT
 
 const startServer = async () => {
-  const apolloServer = new ApolloServer({
-    typeDefs,
-    resolvers,
-    context: async ({ req }) => {
-      return {
-        loggedInUser: await getUser(req.headers.token),
-      };
-    },
-    plugins: [ApolloServerPluginLandingPageGraphQLPlayground()],
-  });
+    const apolloServer = new ApolloServer({
+        typeDefs,
+        resolvers,
+        context: async ({ req }) => {
+            return {
+                loggedInUser: await getUser(req.headers.token),
+            }
+        },
+        plugins: [ApolloServerPluginLandingPageGraphQLPlayground()],
+    })
 
-  await apolloServer.start();
+    await apolloServer.start()
 
-  const app = express();
-  // app.use(logger("tiny"))
+    const app = express()
+    // app.use(logger("tiny"))
 
-  app.use(graphqlUploadExpress());
+    app.use(graphqlUploadExpress())
 
-  apolloServer.applyMiddleware({ app });
+    apolloServer.applyMiddleware({ app })
 
-  app.use("/static", express.static("uploads"));
+    app.use('/static', express.static('uploads'))
 
-  await new Promise((r) => app.listen({ port: PORT }, r));
+    await new Promise((r) => app.listen({ port: PORT }, r))
 
-  console.log(
-    `🚀 apolloServer ready at http://localhost:${PORT}${apolloServer.graphqlPath}`
-  );
-};
+    console.log(
+        `🚀 apolloServer ready at http://localhost:${PORT}${apolloServer.graphqlPath}`
+    )
+}
 
-startServer();
-
+startServer()
