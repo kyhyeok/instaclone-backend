3cb858c0f812d60dd3c146542f7da2e2ecd5a9fd
김윤혁 <kyhyeok@gmail.com>
2021-11-18 13:59:20 +0900

    me return

--- users/me/me.resolvers.js
+++ users/me/me.resolvers.js
@@ -3,12 +3,12 @@ import { protectedResolver } from "../users.utils";
 
 export default {
     Query: {
-        me: protectedResolver((_, __, { loggedInUser }) => {
+        me: protectedResolver((_, __, { loggedInUser }) =>
             client.user.findUnique({
                 where: {
                     id: loggedInUser.id,
                 },
-            });
-        }),
+            })
+        ),
     },
 };
