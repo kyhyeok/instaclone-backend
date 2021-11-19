2315fa4230f8b795ac150295d01b62ebde9f83fa
김윤혁 <kyhyeok@gmail.com>
2021-11-19 14:28:10 +0900

    photo add isLiked

--- photos/photos.typeDefs.js
+++ photos/photos.typeDefs.js
@@ -1,30 +1,31 @@
-import { gql } from 'apollo-server';
+import { gql } from "apollo-server";
 
 export default gql`
-    type Photo {
-        id: Int!
-        user: User!
-        file: String!
-        caption: String
-        likes: Int!
-        hashtags: [Hashtag]
-        isMine: Boolean!
-        comments: Int!
-        createdAt: String!
-        updatedAt: String!
-    }
-    type Hashtag {
-        id: Int!
-        hashtag: String!
-        photos(page: Int!): [Photo]
-        totalPhotos: Int!
-        createdAt: String!
-        updatedAt: String!
-    }
-    type Like {
-        id: Int!
-        photo: Photo!
-        createdAt: String!
-        updatedAt: String!
-    }
+  type Photo {
+    id: Int!
+    user: User!
+    file: String!
+    caption: String
+    likes: Int!
+    hashtags: [Hashtag]
+    isMine: Boolean!
+    isLiked: Boolean!
+    comments: Int!
+    createdAt: String!
+    updatedAt: String!
+  }
+  type Hashtag {
+    id: Int!
+    hashtag: String!
+    photos(page: Int!): [Photo]
+    totalPhotos: Int!
+    createdAt: String!
+    updatedAt: String!
+  }
+  type Like {
+    id: Int!
+    photo: Photo!
+    createdAt: String!
+    updatedAt: String!
+  }
 `;
